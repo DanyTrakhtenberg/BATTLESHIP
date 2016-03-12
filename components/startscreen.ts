8c2687d08c397704819ecfa5589432d13a6507a0
stlegek <kyle.st.leger.barter@autodesk.com>
2016-03-12 13:27:24 -0800

    switched blinking, fixing font-size

--- components/startscreen.ts
+++ components/startscreen.ts
@@ -1,8 +1,8 @@
 class StartScreen {
     public static template = 
     '<div class="start-screen">' +
-    '<h3 class="press-start blink_me">PRESS START</h3>' +
-    '<div class="start-button">' +
+    '<h3 class="press-start">PRESS START</h3>' +
+    '<div class="start-button blink_me">' +
     '<a v-on:click="start" class="pure-button pure-button-primary" href="#">Start</a>' +
     '</div>' +
     '</div>';
